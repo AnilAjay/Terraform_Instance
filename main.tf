5925199aa9d71922fcf092220e3af684a0063b7c
anil <ravulakumar57@gmail.com>
2025-04-01 16:13:52 +0530

    Second

--- main.tf
+++ main.tf
@@ -5,10 +5,11 @@ provider "aws" {
 
 # EC2 Instance Resource
 resource "aws_instance" "example" {
-  ami           = "ami-071226ecf16aa7d96"  # Replace with a valid AMI ID in your region
+  ami           = "ami-071226ecf16aa7d96" 
   instance_type = "t2.micro"  # Adjust the instance type based on your needs
 
   tags = {
     Name = "ExampleInstance"
+    server = "sample"
   }
 }
