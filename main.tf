a6594c6d033c6a9855a8889c2a75abc64c080a9a
AnilAjay <130689593+AnilAjay@users.noreply.github.com>
2025-04-02 23:16:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,91 @@
-# Provider Configuration
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "us-east-1"
 }
 
-# EC2 Instance Resource
-resource "aws_instance" "example" {
-  ami           = "ami-071226ecf16aa7d96" 
-  instance_type = "t2.micro"  # Adjust the instance type based on your needs
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-00a929b66ed6e0de6"
+  instance_type = "t2.micro"
+  key_name      = "test_keypair"
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > /var/www/html/index.html
+              sudo yum install -y httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              EOF
 
   tags = {
-    Name = "ExampleInstance"
-    server = "sample"
+    Name = "WebServer"
   }
+}
+
+resource "aws_lb" "web_lb" {
+  name               = "web-loadbalancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups   = [aws_security_group.web_sg.id]
+  subnets           = ["subnet-0be9d6cd4c51c397e", "subnet-0915df8c4a7c67362"]
+
+  enable_deletion_protection = false
+  enable_cross_zone_load_balancing = true
+}
+
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-03f701a996b62f554"
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      status_code = 200
+      message_body = "Hello, World!"
+      content_type = "text/plain"
+    }
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_target_group_attachment" {
+  target_group_arn = aws_lb_target_group.web_target_group.arn
+  target_id        = aws_instance.webserver.id
+  port             = 80
+}
+
+output "load_balancer_url" {
+  value = aws_lb.web_lb.dns_name
 }
